0fc5de5337757783cc228073e696daab6d42afef
raja1802 <rajagandla16@gmail.com>
2022-11-12 23:39:16 +0530

    asdhgahds

--- axios.mjs
+++ axios.mjs
@@ -3,39 +3,39 @@ import { MongoClient } from "mongodb";
 const pass = encodeURIComponent("Raja@1802");
 var url = `mongodb://ajar:${pass}@cluster0-shard-00-00.jomxs.mongodb.net:27017,cluster0-shard-00-01.jomxs.mongodb.net:27017,cluster0-shard-00-02.jomxs.mongodb.net:27017/?ssl=true&replicaSet=atlas-nv3wvh-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
-function sleep(ms) {
+async function sleep(ms) {
   console.log("waiting...");
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-function baseConvert(sttr) {
+async function baseConvert(sttr) {
   var b64string = sttr;
   //   console.log(sttr);
   var buf = Buffer.from(b64string, "base64").toString("utf-8");
   console.log(buf);
 }
 
-function splitter(str) {
+async function splitter(str) {
   console.log(str);
   if (!str.endsWith(".mp4")) {
     var lista = str.split("#");
-    baseConvert(lista[1]);
+    await baseConvert(lista[1]);
   }
 }
-function axio(id) {
+async function axio(id) {
   var uri = `https://animixplay.to${id}`;
   //   console.log(id);
   sleep(2000);
   try {
-    axios.get(uri).then((resp) => {
+    await axios.get(uri).then((resp) => {
       var str = resp.request.res.responseUrl;
-      splitter(str);
+      await splitter(str);
     });
   } catch (error) {
     console.log("leg in ditch");
   }
 }
-MongoClient.connect(url, function (err, db) {
+MongoClient.connect(url, async function (err, db) {
   if (err) throw err;
   var dbo = db.db("animixplay");
   dbo
@@ -52,7 +52,7 @@ MongoClient.connect(url, function (err, db) {
           !element.player_url.startsWith("https://")
         ) {
           try {
-            axio(element.player_url);
+            await saxio(element.player_url);
             // console.log(element.player_url);
           } catch (error) {
             console.log("gone");
